fec2b8fe6d76f336a1a7b9fb34625f8db9937eb9
DecksDark <patrickdiazgrandez@gmail.com>
2026-02-06 10:42:13 +0100

    first commit 2

--- scripts/hammer.gd
+++ scripts/hammer.gd
@@ -3,15 +3,15 @@ extends Node2D
 @onready var game: Node2D = $".."
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-signal key_pressed
+#signal key_pressed
 
-func _input(event: InputEvent):
-	if !game.input_locked:
-		if event.is_action_pressed("ui_accept"):
-			key_pressed.emit(1)
-			animation_player.play("hit")
-		elif event.is_action_released("ui_accept"):
-			animation_player.play("release")
+#func _input(event: InputEvent):
+	#if !game.input_locked:
+		#if event.is_action_pressed("ui_accept"):
+			#key_pressed.emit(1)
+			#animation_player.play("hit")
+		#elif event.is_action_released("ui_accept"):
+			#animation_player.play("release")
 		#if(event.is_action_pressed("rhythm_game_key_2")):
 			#key_pressed.emit(2)
 		#if(event.is_action_pressed("rhythm_game_key_3")):
--- song_player.gd
+++
@@ -1,7 +0,0 @@
-extends AudioStreamPlayer
-
-const START_AT_TICK:int = 0
-
-func _on_game_clock_tick(current_tick:int) -> void:
-	if !playing and current_tick == START_AT_TICK:
-		play()
